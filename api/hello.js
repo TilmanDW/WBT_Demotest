d4ea3776ae0fe0288f54ca9b7b3b033148ab5109
TIlMan <88382494+TilmanDW@users.noreply.github.com>
2025-07-25 12:50:10 +0200

    Delete api directory

--- api/hello.js
+++
@@ -1,4 +0,0 @@
-// api/hello.js - Using CommonJS syntax
-module.exports = function handler(req, res) {
-  res.status(200).json({ message: 'Hello, this API route is working!' });
-};
--- api/test.js
+++
@@ -1,4 +0,0 @@
-// api/test.js - Minimal example
-module.exports = function(req, res) {
-  res.status(200).json({ message: 'API is working' });
-};
